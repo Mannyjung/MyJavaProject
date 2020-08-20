4e57634c6d89ac4a538db3dded41e86342a5b197
mannini <614259041@webmail.npru.ac.th>
2020-08-20 11:22:06 +0700

    update text

--- Hello.java
+++ Hello.java
@@ -1,8 +1,6 @@
 public class Hello{
 		public static void main(String[] args){
-			System.out.println("Muangman Srathongjeen");
-System.out.println("Muangman Srathongjeen");
-System.out.println("Mannyyyyyyyyyyyyyyyyyy");
+			System.out.println("Software");
 
 	}
 }
